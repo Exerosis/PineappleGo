330038be827a6a81aa50e1ad7290acc34ca7ec4d
Exerosis <exerosis@gmail.com>
2023-04-03 01:41:58 -0400

    ~ Starting to benchmark.

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"github.com/exerosis/PineappleGo/pineapple"
 	"net"
 	"strings"
+	"sync"
 	"time"
 )
 
@@ -61,13 +62,19 @@ func run() error {
 
 	var start = time.Now()
 	var count = 10_000
+	var group sync.WaitGroup
+	group.Add(count)
 	for i := 0; i < count; i++ {
-		var cas = pineapple.NewCas([]byte("world"), []byte("universe"))
-		reason = node.ReadModifyWrite([]byte("hello"), cas)
-		if reason != nil {
-			return reason
-		}
+		go func() {
+			defer group.Done()
+			var cas = pineapple.NewCas([]byte("world"), []byte("universe"))
+			reason = node.ReadModifyWrite([]byte("hello"), cas)
+			if reason != nil {
+				panic(reason)
+			}
+		}()
 	}
+	group.Wait()
 	var took = float64(count) / time.Since(start).Seconds()
 	fmt.Printf("%0.2f ops/s\n", took)
 
